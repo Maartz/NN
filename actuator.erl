2171851312cd18b445711e6c5a710a52c3b6d2f1
William Martin <maartz@icloud.com>
2025-05-25 09:02:15 +0200

    wip benchmark

--- actuator.erl
+++ actuator.erl
@@ -2,33 +2,36 @@
 -compile(export_all).
 -include("records.hrl").
 
-gen(ExoSelfPId, Node) ->
-    spawn(Node, ?MODULE, loop, [ExoSelfPId]).
+gen(ExoSelf_PId,Node)->
+	spawn(Node,?MODULE,prep,[ExoSelf_PId]).
 
-loop(ExoSelfPId) ->
-    receive
-        {ExoSelfPId, {Id, CortexPId, ActuatorName, FaninPIds}} ->
-            loop(Id, CortexPId, ActuatorName, {FaninPIds, FaninPIds}, [])
-    end.
+prep(ExoSelf_PId) -> 
+	receive 
+		{ExoSelf_PId,{Id,Cx_PId,Scape,ActuatorName,Fanin_PIds}} ->
+			loop(Id,ExoSelf_PId,Cx_PId,Scape,ActuatorName,{Fanin_PIds,Fanin_PIds},[])
+	end.
 
-loop(Id, CortexPId, ActuatorName, {[FromPId | FaninPIds], MFaninPIds}, Acc) ->
-    receive
-        {FromPId, forward, Input} ->
-            loop(Id, CortexPId, ActuatorName, {FaninPIds, MFaninPIds}, lists:append(Input, Acc));
-        {CortexPId, terminate} ->
-            ok
-        end;
-loop(Id, CortexPId, ActuatorName, {[], MFaninPIds}, Acc) ->
-    actuator:ActuatorName(lists:reverse(Acc)),
-    CortexPId ! {self(), sync},
-    loop(Id, CortexPId, ActuatorName, {MFaninPIds, MFaninPIds}, []).
+loop(Id,ExoSelf_PId,Cx_PId,Scape,AName,{[From_PId|Fanin_PIds],MFanin_PIds},Acc) ->
+	receive
+		{From_PId,forward,Input} ->
+			loop(Id,ExoSelf_PId,Cx_PId,Scape,AName,{Fanin_PIds,MFanin_PIds},lists:append(Input,Acc));
+		{ExoSelf_PId,terminate} ->
+			ok
+	end;
+loop(Id,ExoSelf_PId,Cx_PId,Scape,AName,{[],MFanin_PIds},Acc)->
+	{Fitness,EndFlag} = actuator:AName(lists:reverse(Acc),Scape),
+	Cx_PId ! {self(),sync,Fitness,EndFlag},
+	loop(Id,ExoSelf_PId,Cx_PId,Scape,AName,{MFanin_PIds,MFanin_PIds},[]).
 
-pts(Result) ->
-    io:format("actuators:pts(Result): ~p~n", [Result]).
+
+pts(Result,_Scape)->
+	io:format("actuator:pts(Result): ~p~n",[Result]),
+	{1,0}.
+%The pts/2 actuation function simply prints to screen the vector passed to it.
 
 xor_SendOutput(Output,Scape)->
-    Scape ! {self(),action,Output},
-    receive
-    {Scape,Fitness,HaltFlag}->
-    {Fitness,HaltFlag}
-    end.
+	Scape ! {self(),action,Output},
+	receive 
+		{Scape,Fitness,HaltFlag}->
+			{Fitness,HaltFlag}
+	end.
